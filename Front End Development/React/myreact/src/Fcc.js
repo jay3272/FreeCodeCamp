ad1b72eed1645977f1ff26f8212bba4d029f6b5d
吳介 <jaywu3272@gmail.com>
2024-11-28 05:21:45 +0000

    創建一個 React 組件

--- Front End Development/React/myreact/src/Fcc.js
+++ Front End Development/React/myreact/src/Fcc.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
-const MyComponent = function() {  
- return(
-    <div>Completed challenge!</div>
-);
-}
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+return (
+      <div>
+       <h1>Hello React!</h1>
+      </div>
+    );
+  }
+};
 
 function Fcc() {
   return (
     <div>
-      <h1>Welcome to the FCC Homepage!</h1>
       {/* 渲染 MyComponent */}
       <MyComponent />
     </div>
