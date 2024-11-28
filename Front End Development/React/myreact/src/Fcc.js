c20967b4e85977a16b6387d86c60e604ab08f8a7
吳介 <jaywu3272@gmail.com>
2024-11-28 09:07:22 +0000

    使用生命週期方法 componentWillMount

--- Front End Development/React/myreact/src/Fcc.js
+++ Front End Development/React/myreact/src/Fcc.js
@@ -1,67 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class MyApp extends React.Component {
+class MyComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      inputValue: ''
-    };
-      this.handleChange = this.handleChange.bind(this);
   }
-
-  handleChange(event){
-    this.setState({
-      inputValue: event.target.value
-    })
+  componentWillMount() {
+  console.log('Component being mounted');
   }
-  
   render() {
-    return (
-      <div>
-        <GetInput input={this.state.inputValue}
-        handleChange={this.handleChange} />
-        <RenderInput
-        input={this.state.inputValue} />
-      </div>
-    );
+    return <div />
   }
 };
 
-class GetInput extends React.Component{
-  constructor(props){
-    super(props);
-  }
-render(){
-  return(
-    <div>
-      <h3>Get Input:</h3>
-      <input 
-        value={this.props.input}
-        onChange={this.props.handleChange}/>
-    </div>
-  )
-}
-}
-
-class RenderInput extends React.Component{
-  constructor(props){
-    super(props);
-  }
-  render(){
-    return(
-      <div>
-        <h3>Input Render:</h3>
-        <p>{this.props.input}</p>
-      </div>
-    )
-  }
-}
 
 function Fcc() {
   return (
     <div>
-      <MyApp />
+      <MyComponent />
     </div>
   );
 }
