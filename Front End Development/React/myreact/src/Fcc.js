699c8fd013443cfcd4c8b1e66001df84c8e84475
吳介 <jaywu3272@gmail.com>
2024-11-28 09:55:20 +0000

    介紹內聯樣式

--- Front End Development/React/myreact/src/Fcc.js
+++ Front End Development/React/myreact/src/Fcc.js
@@ -1,56 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class OnlyEvens extends React.Component {
-  constructor(props) {
-    super(props);
-    }
-    shouldComponentUpdate(nextProps, nextState){
-      console.log('Should I update?');
-      if (nextProps.value % 2 == 0) {
-        return true;
-      }
-      return false;
-    }
-    componentDidUpdate(){
-      console.log('Component re-rendered.')
-    }
+class Colorful extends React.Component {
   render() {
     return (
-      <div>
-        <h1>{this.props.value}</h1>
-      </div>
+      <div style={{color: "red", fontSize: 72}}>Big Red</div>
     )
-  }
-};
-
-class Controller extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 0
-    };
-    this.addValue = this.addValue.bind(this);
-  }
-  addValue() {
-    this.setState(state => ({
-      value: state.value + 1
-    }));
-  }
-  render() {
-    return (
-      <div>
-        <button onClick={this.addValue}>Add</button>
-        <OnlyEvens value={this.state.value} />
-      </div>
-    );
   }
 }
 
 function Fcc() {
   return (
     <div>
-      <Controller />
+      <Colorful />
     </div>
   );
 }
