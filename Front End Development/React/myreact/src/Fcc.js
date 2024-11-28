9349ed5cbc9badf41b34fd923b12c3911570afd3
吳介 <jaywu3272@gmail.com>
2024-11-28 06:06:08 +0000

    Pass an Array as Props

--- Front End Development/React/myreact/src/Fcc.js
+++ Front End Development/React/myreact/src/Fcc.js
@@ -1,22 +1,21 @@
 import React from 'react';
 
-const CurrentDate = (props) => {
-  return (
-    <div>
-      <p>The current date is: {props.date}</p>
-    </div>
-  );
+const List = (props) => {
+  return <p>{props.tasks.join(", ")}</p>
 };
 
-class Calendar extends React.Component {
+class ToDo extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
       <div>
-        <h3>What date is it?</h3>
-        <CurrentDate date={Date()} />
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
+        <List tasks={["Walk", "Cook", "Bake"]} />
+        <h2>Tomorrow</h2>
+        <List tasks={["Study", "Code", "Eat"]} />
       </div>
     );
   }
@@ -25,7 +24,7 @@ class Calendar extends React.Component {
 function Fcc() {
   return (
     <div>
-      <Calendar />
+      <ToDo />
     </div>
   );
 }
