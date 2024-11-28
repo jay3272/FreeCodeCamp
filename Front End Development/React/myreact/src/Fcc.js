7d68f1509510076fe2df86a42a587573bcc7be38
吳介 <jaywu3272@gmail.com>
2024-11-28 07:18:20 +0000

    複習使用無狀態函數組件的 Props

--- Front End Development/React/myreact/src/Fcc.js
+++ Front End Development/React/myreact/src/Fcc.js
@@ -1,23 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-class Welcome extends React.Component {
+class CampSite extends React.Component {
   constructor(props) {
     super(props);
-
   }
   render() {
     return (
-        <div>
-          <p>Hello, <strong>{this.props.name}</strong>!</p>
-        </div>
+      <div>
+        <Camper name={this.props.name}/>
+      </div>
     );
   }
+};
+
+const Camper = props => <p>{props.name}</p>;
+
+Camper.defaultProps = {
+  name: "CamperBot"
+};
+
+Camper.propTypes = {
+  name: PropTypes.string.isRequired
 };
 
 function Fcc() {
   return (
     <div>
-      <Welcome name="Jessica"/>
+      <CampSite />
     </div>
   );
 }
