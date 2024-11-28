b0b6cd6408d073b2a4bbe0f234aab3e66c60422b
吳介 <jaywu3272@gmail.com>
2024-11-28 09:01:50 +0000

    傳遞迴調作爲 Props

--- Front End Development/React/myreact/src/Fcc.js
+++ Front End Development/React/myreact/src/Fcc.js
@@ -5,30 +5,58 @@ class MyApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'CamperBot'
+      inputValue: ''
     };
+      this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({
+      inputValue: event.target.value
+    })
   }
   
   render() {
     return (
       <div>
-        <Navbar name = {this.state.name}/>
+        <GetInput input={this.state.inputValue}
+        handleChange={this.handleChange} />
+        <RenderInput
+        input={this.state.inputValue} />
       </div>
     );
   }
 };
 
-class Navbar extends React.Component{
+class GetInput extends React.Component{
   constructor(props){
-    super(props)
+    super(props);
   }
-render() {
+render(){
   return(
     <div>
-      <h1>Hello, my name is: {this.props.name}</h1>
+      <h3>Get Input:</h3>
+      <input 
+        value={this.props.input}
+        onChange={this.props.handleChange}/>
     </div>
   )
-}}
+}
+}
+
+class RenderInput extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div>
+        <h3>Input Render:</h3>
+        <p>{this.props.input}</p>
+      </div>
+    )
+  }
+}
 
 function Fcc() {
   return (
