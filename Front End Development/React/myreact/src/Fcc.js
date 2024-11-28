699695935993d9d27c4d20f994e3ea43f4f8825f
吳介 <jaywu3272@gmail.com>
2024-11-28 06:01:11 +0000

    將 Props 傳遞給無狀態函數組件

--- Front End Development/React/myreact/src/Fcc.js
+++ Front End Development/React/myreact/src/Fcc.js
@@ -1,67 +1,22 @@
 import React from 'react';
 
-const NonCitrus = () => {
+const CurrentDate = (props) => {
   return (
     <div>
-      <h3>Non-Citrus:</h3>
-      <ul>
-        <li>Apples</li>
-        <li>Blueberries</li>
-        <li>Strawberries</li>
-        <li>Bananas</li>
-      </ul>
+      <p>The current date is: {props.date}</p>
     </div>
   );
 };
 
-const Citrus = () => {
-  return (
-    <div>
-      <h3>Citrus:</h3>
-      <ul>
-        <li>Lemon</li>
-        <li>Lime</li>
-        <li>Orange</li>
-        <li>Grapefruit</li>
-      </ul>
-    </div>
-  );
-};
-
-const Vegetables = () => {
-  return (
-    <div>
-      <h2>Vegetables:</h2>
-      <ul>
-        <li>Brussel Sprouts</li>
-        <li>Broccoli</li>
-        <li>Squash</li>
-      </ul>
-    </div>
-  );
-};
-
-const Fruits = () => {
-  return (
-    <div>
-      <h2>Fruits:</h2>
-        <NonCitrus />
-        <Citrus />
-    </div>
-  );
-};
-
-class TypesOfFood extends React.Component {
+class Calendar extends React.Component {
   constructor(props) {
     super(props);
   }
-
   render() {
     return (
       <div>
-        <h1>Types of Food:</h1>
-        <Fruits/>
-        <Vegetables />
+        <h3>What date is it?</h3>
+        <CurrentDate date={Date()} />
       </div>
     );
   }
@@ -70,7 +25,7 @@ class TypesOfFood extends React.Component {
 function Fcc() {
   return (
     <div>
-      <TypesOfFood />
+      <Calendar />
     </div>
   );
 }
