86764cdb2e8de65c56d43390a4285f33bc781e19
吳介 <jaywu3272@gmail.com>
2024-11-28 06:56:05 +0000

    使用 this.props 訪問 Props

--- Front End Development/React/myreact/src/Fcc.js
+++ Front End Development/React/myreact/src/Fcc.js
@@ -1,32 +1,23 @@
 import React from 'react';
 
-const Items = (props) => {
-  return (
-    <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
-  )
-};
-
-Items.propTypes = {
-  quantity: PropTypes.number.isRequired
-};
-
-Items.defaultProps = {
-  quantity: 0
-};
-
-class ShoppingCart extends React.Component {
+class Welcome extends React.Component {
   constructor(props) {
     super(props);
+
   }
   render() {
-    return <Items quantity={10}/>
+    return (
+        <div>
+          <p>Hello, <strong>{this.props.name}</strong>!</p>
+        </div>
+    );
   }
 };
 
 function Fcc() {
   return (
     <div>
-      <ShoppingCart />
+      <Welcome name="Jessica"/>
     </div>
   );
 }
