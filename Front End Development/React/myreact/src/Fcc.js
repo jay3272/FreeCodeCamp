2e5a7cbf4ba47a807b2f6584a8e44db3187460dd
吳介 <jaywu3272@gmail.com>
2024-11-28 10:00:07 +0000

    在 React 中添加內聯樣式

--- Front End Development/React/myreact/src/Fcc.js
+++ Front End Development/React/myreact/src/Fcc.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-class Colorful extends React.Component {
-  render() {
-    return (
-      <div style={{color: "red", fontSize: 72}}>Big Red</div>
+const styles ={
+  color: 'purple',
+  fontSize: 40,
+  border: "2px solid purple",
+}
+
+class Colorful extends React.Component{
+  render(){
+    return(
+      <div style={styles}>Style Me!</div>
     )
   }
 }
