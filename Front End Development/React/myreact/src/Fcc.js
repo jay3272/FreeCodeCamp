a35b838dccb072b83215dd86fcf7016cc60bca30
吳介 <jaywu3272@gmail.com>
2024-11-28 09:28:48 +0000

    添加事件偵聽器

--- Front End Development/React/myreact/src/Fcc.js
+++ Front End Development/React/myreact/src/Fcc.js
@@ -5,20 +5,33 @@ class MyComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      activeUsers: null
+      message: ''
     }
+    this.handleEnter = this.handleEnter.bind(this); 
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
   componentDidMount() {
-    setTimeout(() =>{
-      this.setState({
-        activeUsers: 1273
-      })
-    }, 2500)
+    document.addEventListener("keydown",this.handleKeyPress);
   }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+  handleEnter(){
+    this.setState((state) =>({
+      message: state.message + 'Yoou presses the enter key!'
+    }))
+  }
+
+  handleKeyPress(event){
+    if(event.keyCode === 13){
+      this.handleEnter();
+    }
+  }
+
   render() {
     return (
       <div>
-        <h1>Active Users: {this.state.activeUsers}</h1>
+        <h1>{this.state.message}</h1>
       </div>
     )
   }
