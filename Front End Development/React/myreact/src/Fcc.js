308b93c106da3141c003802b0cdc9c86305387a2
吳介 <jaywu3272@gmail.com>
2024-11-28 08:18:32 +0000

    創建一個可以控制的輸入框

--- Front End Development/React/myreact/src/Fcc.js
+++ Front End Development/React/myreact/src/Fcc.js
@@ -1,39 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Counter extends React.Component {
+class ControlledInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      input: ''
     };
-    this.increment = this.increment.bind(this);
-  this.decrement = this.decrement.bind(this);
-  this.reset = this.reset.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
-  reset(){
-    this.setState(state =>({
-      count: 0
-    }));
-  }
-  increment(){
-    this.setState(state =>({
-      count: state.count+1
-    }));
-  }
-  decrement(){
-    this.setState(state =>({
-      count: state.count-1
-    }));
+  
+  handleChange(event){
+    this.setState({
+      input: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
-        <button className='inc' onClick={this.increment}>Increment!</button>
-        <button className='dec' onClick={this.decrement}>Decrement!</button>
-        <button className='reset' onClick={this.reset}>Reset</button>
-        <h1>Current Count: {this.state.count}</h1>
+        <input value={this.state.input} onChange={this.handleChange}/>
+        <h4>Controlled Input</h4>
+        <p>{this.state.input}</p>
       </div>
     );
   }
@@ -42,7 +30,7 @@ class Counter extends React.Component {
 function Fcc() {
   return (
     <div>
-      <Counter />
+      <ControlledInput />
     </div>
   );
 }
