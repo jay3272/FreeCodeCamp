7f2a22455753edc2987834240899410662ec5e87
吳介 <jaywu3272@gmail.com>
2024-11-29 02:01:52 +0000

    使用三元表達式進行條件渲染

--- Front End Development/React/myreact/src/Fcc.js
+++ Front End Development/React/myreact/src/Fcc.js
@@ -1,32 +1,63 @@
 import React from 'react';
 
-class MMyComponent extends React.Component {
-  constructor(props) {
+const inputStyle = {
+  width: 235,
+  margin: 5
+};
+
+class CheckUserAge extends React.Component {
+  constructor(props){
     super(props);
-    this.state = {
-      display: true
-    }
-    this.toggleDisplay= this.toggleDisplay.bind(this);
+    this.state ={
+      userAge: '',
+      input: ''
+      }
+      this.submit = this.submit.bind(this);
+      this.handleChange = this.handleChange.bind(this);
   }
-  toggleDisplay(){
-    this.setState((state)=>({
-      display: !state.display
-    }))
-  }
-  render(){
-      return(
-        <div>
-          <button onClick={this.toggleDisplay}>Toggle Display</button>
-          {this.state.display && <h1>Displayed</h1>}
-        </div>
-      )
-  }
+
+handleChange(e) {
+  this.setState({
+    input: e.target.value,
+    userAge: ''
+  });
+}
+
+submit() {
+  this.setState(state =>({
+    userAge: state.input
+  }))
+}
+render() {
+  const buttonOne = <button onClick={this.submit}>Submit</button>;
+  const buttonTwo = <button>You May Enter</button>
+  const buttonThree = <button>You Shall Not Pass</button>
+  return (
+    <div>
+      <h3>Enter Your Age to Continue</h3>
+      <input
+        style={inputStyle}
+        type='number'
+        value={this.state.input}
+        onChange={this.handleChange} 
+      />
+      <br />
+      {
+        this.state.userAge === ''
+          ? buttonOne
+          : this.state.userAge >= 18
+            ? buttonTwo
+            : buttonThree
+      }
+    </div>
+  )
+}
 }
 
 function Fcc() {
   return (
     <div>
-      <MMyComponent />
+      <CheckUserAge />
     </div>
   );
 }
