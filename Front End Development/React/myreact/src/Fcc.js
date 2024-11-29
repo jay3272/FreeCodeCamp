737ace4841675d4aabc68267195f583c032d7f96
吳介 <jaywu3272@gmail.com>
2024-11-29 02:48:22 +0000

    給同級元素一個唯一的鍵屬性

--- Front End Development/React/myreact/src/Fcc.js
+++ Front End Development/React/myreact/src/Fcc.js
@@ -1,54 +1,32 @@
 import React from 'react';
 
-const textAreaStyles = {
-  width: 235,
-  margin: 5
-}
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
 
-class MyToDoList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userInput: '',
-      toDoList: []
-    }
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleSubmit() {
-    const itemArray = this.state.userInput.split(',');
-    this.setState({
-      toDoList: itemArray
-    })
-  }
-  handleChange(e) {
-    this.setState({
-      userInput: e.target.value
-    })
-  }
-  render() {
-    const items = this.state.toDoList.map(i => <li>{i}</li>);
-    return (
-      <div>
-        <textarea 
-          onChange={this.handleChange}
-          value={this.state.userInput}
-          style={textAreaStyles}
-          placeholder='Seprate Items With Commas'
-        />
-        <br />
-        <button onClick={this.handleSubmit}>Create List</button>
-        <h1>My "To Do" List:</h1>
-        <ul>{items}</ul>
-      </div>
-    )
-  }
-}
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map((item) =>
+  <li key={item}>{item}</li>
+); // 修改這一行
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
 
 function Fcc() {
   return (
     <div>
-      <MyToDoList />
+      <Frameworks />
     </div>
   );
 }
