9ff0f426e48388b5a960f099b435aeb841078cd5
吳介 <jaywu3272@gmail.com>
2024-11-29 02:25:45 +0000

    Render Conditionally from Props

--- Front End Development/React/myreact/src/Fcc.js
+++ Front End Development/React/myreact/src/Fcc.js
@@ -1,63 +1,52 @@
 import React from 'react';
 
-const inputStyle = {
-  width: 235,
-  margin: 5
-};
-
-class CheckUserAge extends React.Component {
+class Results extends React.Component {
   constructor(props){
     super(props);
-    this.state ={
-      userAge: '',
-      input: ''
-      }
-      this.submit = this.submit.bind(this);
-      this.handleChange = this.handleChange.bind(this);
-  }
-
-handleChange(e) {
-  this.setState({
-    input: e.target.value,
-    userAge: ''
-  });
-}
-
-submit() {
-  this.setState(state =>({
-    userAge: state.input
-  }))
-}
+  }  
 render() {
-  const buttonOne = <button onClick={this.submit}>Submit</button>;
-  const buttonTwo = <button>You May Enter</button>
-  const buttonThree = <button>You Shall Not Pass</button>
   return (
-    <div>
-      <h3>Enter Your Age to Continue</h3>
-      <input
-        style={inputStyle}
-        type='number'
-        value={this.state.input}
-        onChange={this.handleChange} 
-      />
-      <br />
-      {
-        this.state.userAge === ''
-          ? buttonOne
-          : this.state.userAge >= 18
-            ? buttonTwo
-            : buttonThree
-      }
-    </div>
+    <h1>
+      {this.props.fiftyFifty ? "You Win!" : "You Lose!"}
+    </h1>
   )
 }
 }
 
+class GameOfChance extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 1
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  
+  handleClick() {
+    this.setState(prevState => {
+      return {
+        counter: prevState.counter + 1
+      }
+    })
+  }
+
+  render() {
+    const expression = Math.random() >= 0.5;
+    return (
+      <div>
+        <button onClick={this.handleClick}>Play Again</button>
+        <Results fiftyFifty={expression} />
+        <p>{'Turn: ' + this.state.counter}</p>
+      </div>
+    )
+  }
+}
+
+
 function Fcc() {
   return (
     <div>
-      <CheckUserAge />
+      <GameOfChance />
     </div>
   );
 }
